8a7bcd9fc7d2d701bb2f34f9bd5097dc646fc7c2
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-07-21 22:09:07 -0300

    feat: principio da autovalidação

--- src/entities/customer.ts
+++ src/entities/customer.ts
@@ -1,20 +1,30 @@
 export default class Customer {
   _id: string
-  _name: string
-  _address: string
-  _active: boolean = true
+  _name: string = ''
+  _address: string = ''
+  _active: boolean = false
 
-  constructor (id: string, name: string, address: string) {
+  constructor (id: string, name: string) {
     this._id = id
     this._name = name
-    this._address = address
+    this.validate()
+  }
+
+  validate (): void {
+    if (!this._id) throw new Error('Customer id is required')
+
+    if (!this._name) throw new Error('Customer name is required')
+
+    if (!this._address) throw new Error('Customer address is required')
   }
 
   changeName (name: string): void {
     this._name = name
+    this.validate()
   }
 
   activate (): void {
+    if (!this._address) throw new Error('Address is mandatory to activate customer')
     this._active = true
   }
 
