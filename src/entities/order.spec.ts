f054092e9d1a5320a65e5fc4203f462f67aab542
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-07-24 13:23:38 -0300

    test: testado relacionamento de item com product

--- src/entities/order.spec.ts
+++ src/entities/order.spec.ts
@@ -15,12 +15,20 @@ describe('Order unit tests', () => {
   })
 
   test('should calculate total', () => {
-    const item1 = new OrderItem('1', 'Item 1', 100)
-    const item2 = new OrderItem('1', 'Item 1', 320)
+    const item1 = new OrderItem('1', 'Item 1', 100, '1', 2)
+    const item2 = new OrderItem('1', 'Item 1', 320, '2', 2)
     const order = new Order('1', '1', [item1, item2])
 
     const total = order.total()
 
-    expect(total).toBe(420)
+    expect(total).toBe(840)
+  })
+
+  test('should throw an error if items quantity is less or equal zero', () => {
+    const item1 = new OrderItem('1', 'Item 1', 100, '1', 0)
+    const item2 = new OrderItem('2', 'Item 2', 100, '1', -1)
+
+    expect(() => new Order('1', '1', [item1])).toThrowError('Order items quantity must be greater than zero')
+    expect(() => new Order('1', '1', [item2])).toThrowError('Order items quantity must be greater than zero')
   })
 })
