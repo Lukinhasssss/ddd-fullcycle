1e47c5dfaf71b30820d03f35656517c7fc807a97
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-07-27 19:47:04 -0300

    feat: modelado order e items

---
+++ src/infrastructure/db/sequelize/model/order-model.ts
@@ -0,0 +1,23 @@
+import { BelongsTo, Column, ForeignKey, HasMany, Model, PrimaryKey, Table } from 'sequelize-typescript'
+import CustomerModel from './customer-model'
+import OrderItemModel from './order-item-model'
+
+@Table({ tableName: 'orders', timestamps: false })
+export default class OrderModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string
+
+  @ForeignKey(() => CustomerModel)
+  @Column({ allowNull: false })
+  declare customerId: string
+
+  @BelongsTo(() => CustomerModel)
+  declare customer: CustomerModel // Caso necessário recuperar o cliente associado ao pedido
+
+  @HasMany(() => OrderItemModel) // Indica que o pedido possui vários itens
+  declare items: OrderItemModel[]
+
+  @Column({ allowNull: false })
+  declare total: number
+}
