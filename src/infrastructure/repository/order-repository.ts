2aaf2352eb07c59291e417fee75678e2277b3ca0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-07-28 22:03:21 -0300

    feat: implementado metodo findById de order repository

--- src/infrastructure/repository/order-repository.ts
+++ src/infrastructure/repository/order-repository.ts
@@ -1,4 +1,5 @@
 import Order from '../../domain/entities/order'
+import OrderItem from '../../domain/entities/order_item'
 import OrderRepositoryInterface from '../../domain/repository/order-repository-interface'
 import OrderItemModel from '../db/sequelize/model/order-item-model'
 import OrderModel from '../db/sequelize/model/order-model'
@@ -29,7 +30,22 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async findById (id: string): Promise<Order> {
-    throw new Error('Method not implemented.')
+    const order = await OrderModel.findOne({
+      where: { id },
+      include: [{ model: OrderItemModel }]
+    })
+
+    return new Order(
+      order.id,
+      order.customerId,
+      order.items.map(item => new OrderItem(
+        item.id,
+        item.name,
+        item.price,
+        item.productId,
+        item.quantity
+      ))
+    )
   }
 
   async findAll (): Promise<Order[]> {
