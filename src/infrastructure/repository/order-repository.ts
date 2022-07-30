2446f5fc4e8ccc8fc962af5329bfc899e987e5a6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-07-30 12:50:29 -0300

    feat: implementado metodo update de order repository

--- src/infrastructure/repository/order-repository.ts
+++ src/infrastructure/repository/order-repository.ts
@@ -26,7 +26,28 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async update (entity: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+    const sequelize = OrderModel.sequelize
+
+    await sequelize.transaction(async (t) => {
+      await OrderItemModel.destroy({
+        where: { orderId: entity.id },
+        transaction: t
+      })
+
+      const items = entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        productId: item.productId,
+        quantity: item.quantity,
+        orderId: entity.id
+      }))
+      await OrderItemModel.bulkCreate(items, { transaction: t })
+      await OrderModel.update(
+        { total: entity.total() },
+        { where: { id: entity.id }, transaction: t }
+      )
+    })
   }
 
   async findById (id: string): Promise<Order> {
